de282e61472b1f638b6502ff1d0b96b1d3eb0fb4
tchering <sonam.sherpa.lama@gmail.com>
2024-04-17 21:43:47 +0200

    correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
   address: 'smtp.gmail.com',
   port: 587,
-  domain: 'https://sonam-app.herokuapp.com',
+  domain: 'heroku.com',
   user_name: ENV['GMAIL_USERNAME'],
   password: ENV['GMAIL_PASSWORD'],
   authentication: 'plain',
