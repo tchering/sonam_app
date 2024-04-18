195696e956dbd79031e4b1619a13e5abff8867b9
tchering <sonam.sherpa.lama@gmail.com>
2024-04-18 11:59:42 +0200

    correction to error logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,11 +120,11 @@ Rails.application.configure do
      config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'sonam-app.herokuapp.com'}
   config.action_mailer.smtp_settings = {
-    address: "smtp.mailertogo.com",
+    address: ENV["MAILERTOGO_SMTP_HOST"],
     port: 587,
     domain: "heroku.com",
-    user_name: ENV["MAILERTOGO_USERNAME"],
-    password: ENV["MAILERTOGO_PASSWORD"],
+    user_name: ENV["MAILERTOGO_SMTP_USER"],
+    password: ENV["MAILERTOGO_SMTP_PASSWORD"],
     authentication: "plain",
     enable_starttls_auto: true,
   }
