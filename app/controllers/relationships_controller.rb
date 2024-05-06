afe1965832f285f823c3413e62fdb65be14aed75
tchering <sonam.sherpa.lama@gmail.com>
2024-05-06 10:55:25 +0200

    test

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,11 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:followed_id])
     current_user.follow(@user)
-    redirect_to user_path(@user)
+    # redirect_to user_path(@user)
+    respond_to do |format|
+      format.html { redirect_to user_path(@user) }
+      format.js
+    end
   end
 
   def destroy
