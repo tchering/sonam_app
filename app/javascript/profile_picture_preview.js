753c2a6bba6bc9bf7e54e5945ba6102e43cab0cd
tchering <sonam.sherpa.lama@gmail.com>
2024-05-07 12:59:24 +0200

    corrected profile picture preview error

--- app/javascript/profile_picture_preview.js
+++ app/javascript/profile_picture_preview.js
@@ -1,7 +1,16 @@
-document.getElementById("image_file").onchange = function () {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        document.getElementById("image_preview").src = e.target.result;
-    };
-    reader.readAsDataURL(this.files[0]);
-};
+// document.getElementById("micropost_image_file").onchange = function () {
+//     var reader = new FileReader();
+//     reader.onload = function (e) {
+//         document.getElementById("micropost_image_preview").src = e.target.result;
+//     };
+//     reader.readAsDataURL(this.files[0]);
+// };
+
+
+// document.getElementById("profile_image_file").onchange = function () {
+//     var reader = new FileReader();
+//     reader.onload = function (e) {
+//         document.getElementById("profile_image_preview").src = e.target.result;
+//     };
+//     reader.readAsDataURL(this.files[0]);
+// };
