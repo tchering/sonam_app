d60897ace7ef78fdd621448e399edaa6ae8d0b15
tchering <sonam.sherpa.lama@gmail.com>
2024-06-11 13:49:38 +0200

    before action for edit and update

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: %i[create destroy index]
+  before_action :logged_in_user, only: %i[create destroy index edit update]
 
   def index
     redirect_to root_path
